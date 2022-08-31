cccf63f0e27a288f9f8c3b2bf6979623e48970f4
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-08-30 21:47:47 -0300

    criando coordinator

---
+++ src/router/coordinator.js
@@ -0,0 +1,15 @@
+export const goToPokedexPage = (navigate) => {
+    navigate("/pokedex")
+}
+
+  export const goToDetailPage = (navigate) => {
+    navigate("/detalhes")
+}
+
+export const goToHomePage = (navigate) => {
+      navigate("/")
+}
+  
+export const goToVoltar = (navigate) => {
+      navigate(-1)
+}
