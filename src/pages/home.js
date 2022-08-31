18dd5a0c538d2df464901d73530930006690dea7
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-08-30 21:54:09 -0300

    modificando home

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,9 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function home() {
+    
+
+
     return (
         <div>
             <h1>Essa é a Home</h1>
