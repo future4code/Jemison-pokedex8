89e6b5d0c0c3bfb79671286f8d4129e03ac96da5
Matheus Queiroz dos Santos <matheusqueirozds@gmail.com>
2022-09-02 15:37:29 -0300

    Organizei o App

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import { GlobalStyles } from "./styled";
 
 export default function App() {
     const [pokemons, setPokemons] = useState([]);
-    const [infoPokemons, setinfoPokemons] = useState([]);
-    const [pokedex, setPokex] = useState([]);
+    const [infoPokemons, setInfoPokemons] = useState([]);
+    const [pokedex, setPokedex] = useState([]);
 
     const getAllPokemons = () => {
         axios
@@ -37,7 +37,7 @@ export default function App() {
                         const ordenaListaPokemons = endpoints.sort((a, b) => {
                             return a.id - b.id;
                         });
-                        setinfoPokemons(ordenaListaPokemons);
+                        setInfoPokemons(ordenaListaPokemons);
                     }
                 })
                 .catch((error) => {
@@ -46,12 +46,12 @@ export default function App() {
         });
     }, [pokemons]);
 
-    const capturarPokemon = (pokeAnalizado) => {
-        setinfoPokemons(
-            infoPokemons.filter((pokemon) => pokemon.id !== pokeAnalizado.id) // Aqui chamamos a funçaõ(estado) que atualiza o seu estado, depois filtramos cada item(pokemon), que esta dentro do estado. comparamos o pokemon pelo id, se o id for diferente, a função executado busca esse id
+    const capturarPokemon = (pokeAnalisado) => {
+        setInfoPokemons(
+            infoPokemons.filter((pokemon) => pokemon.id !== pokeAnalisado.id) // Aqui chamamos a função(estado) que atualiza o seu estado, depois filtramos cada item(pokémon), que esta dentro do estado. comparamos o pokémon pelo id, se o id for diferente, a função executado busca esse id
         );
-        setPokex([...pokedex, pokeAnalizado]); // aqui usamos o spred, que guarda o copia o estado inicial do pokedex, e adiciona o pokemon que foi analizado antes
-    }; // passamos essa função via contex para o PokeCard
+        setPokedex([...pokedex, pokeAnalisado]); // aqui usamos o spread, que guarda o copia o estado inicial do pokédex, e adiciona o pokémon que foi analisado antes
+    }; // passamos essa função via context para o PokeCard
 
     return (
         <ContextPokedex.Provider
