b81cfd15c5ef84f85d70f63db3abe23ca834eba6
Matheus Queiroz dos Santos <matheusqueirozds@gmail.com>
2022-09-06 14:50:02 -0300

    Adicionei uma barra de progresso na página de detalhes

---
+++ src/components/StyledProgress.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Progress = styled.div`
+    background: #d8d8d8;
+    border-radius: 20px;
+    max-width: 100%;
+    height: 30px;
+`;
+export const ProgressDone = styled.div`
+    display: flex;
+    background: linear-gradient(to left, #ef5350, #ff9472);
+    box-shadow: 0 3px 3px -5px #ef5350, 0 2px 5px #ef5350;
+    border-radius: 20px;
+    align-items: center;
+    justify-content: center;
+    width: 100px;
+    height: 100%;
+    color: #fff;
+    transition: 1s ease;
+    font-size: 14px;
+`;
