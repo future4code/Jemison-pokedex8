f7ccf6740bd4137913c8706fbbe8868fdd27053c
Matheus Queiroz dos Santos <matheusqueirozds@gmail.com>
2022-09-06 21:44:59 -0300

    Adicionei temas a página de detalhes

--- src/components/detalhes/StyledDetalhes.js
+++ src/components/detalhes/StyledDetalhes.js
@@ -1,15 +1,21 @@
 import styled from "styled-components";
+import img from "../../assets/img/pokeball.svg";
 
 // Estilização da página de descrição do pokemon
+
+export const Body = styled.div`
+    padding: 0 20px 40px 20px;
+`;
 
 export const Titulo = styled.div`
     display: flex;
     align-items: center;
-    justify-content: center;
     gap: 20px;
     font-weight: normal;
     text-transform: capitalize;
     padding: 40px 20px;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+        Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 
     span::before {
         content: "Nº ";
@@ -18,22 +24,93 @@ export const Titulo = styled.div`
 
 export const ContainerDetalhes = styled.div`
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: 1fr 2fr 2fr;
+    align-items: flex-start;
     gap: 40px;
     max-width: 1200px;
     margin: 0 auto;
-
-    h2 {
-        margin-bottom: 20px;
-    }
-
+    padding: 40px;
+    border-radius: 10px;
+    position: relative;
     li {
         font-size: 1.125rem;
-        line-height: 3;
 
         span {
             font-weight: 500;
         }
+    }
+
+    &::before {
+        display: block;
+        content: "";
+        background: url(${img}) no-repeat;
+        background-position: 320px;
+        width: 640px;
+        height: 640px;
+        position: absolute;
+        top: 0;
+        right: 0;
+        opacity: 0.1;
+    }
+
+    @media screen and (max-width: 1000px) {
+        grid-template-columns: 1fr 2fr 1fr;
+    }
+
+    @media screen and (max-width: 800px) {
+        grid-template-columns: 1fr 2fr;
+    }
+
+    @media screen and (max-width: 600px) {
+        grid-template-columns: 1fr;
+    }
+`;
+
+export const CardContent = styled.ul`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    background: #f2f2f2;
+    padding: 20px;
+    border-radius: 10px;
+    z-index: 2;
+
+    h2 {
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+            Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    }
+
+    li {
+        span {
+            font-family: monospace;
+            font-weight: 700;
+        }
+    }
+
+    img {
+        height: auto;
+    }
+`;
+
+export const Tipos = styled.div`
+    li {
+        display: flex;
+        gap: 5px;
+    }
+`;
+
+export const ImagensPokemon = styled.ul`
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    gap: 40px;
+    max-width: 100%;
+
+    @media screen and (max-width: 600px) {
+        flex-wrap: nowrap;
+        flex-direction: row;
     }
 `;
 
@@ -44,56 +121,7 @@ export const Contents = styled.div`
     &:first-child {
         background: blue;
     }
-`;
-
-export const DetailsContent = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 40px;
-    padding: 20px;
-    background: #f2f2f2;
-    border-radius: 10px;
-
-    &:nth-child(1) {
-        display: flex;
+    @media screen and (max-width: 800px) {
+        grid-column: 1/-1;
     }
 `;
-
-export const TypesContent = styled.ul`
-    display: flex;
-    gap: 40px;
-    padding: 20px;
-    background: #f2f2f2;
-    border-radius: 10px;
-`;
-
-export const ImagensPokemon = styled.div``;
-
-export const ImagemPokemon = styled.div`
-    display: flex;
-    justify-content: center;
-    background: #f2f2f2;
-    height: 290px;
-    border-radius: 10px;
-
-    &:first-child {
-        margin-bottom: 40px;
-    }
-`;
-
-export const BaseDeStatus = styled.ul`
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-
-    li {
-        display: grid;
-        grid-template-columns: 1fr 1.5fr;
-        align-items: center;
-        gap: 10px;
-        line-height: 1;
-    }
-`;
-
-export const Habilidades = styled.ul``;
-export const Evolucoes = styled.ul``;
