4565a60611e6a223a14e6ddaf90e73226e9285e5
Matheus Queiroz dos Santos <matheusqueirozds@gmail.com>
2022-09-10 23:41:57 -0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 // O usuário deve receber uma lista de pokemons vinda da API. Ele pode armazenar Pokemons em sua Pokedex clicando em cada um.
-
 import React, { useContext, useState } from "react";
 import { ContextPokedex } from "../context/ContexPokedex";
 import { Body, Tela } from "../styled";
